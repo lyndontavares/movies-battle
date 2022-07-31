3269ce175468b74211fc6f296dc7d0f57ca485af
lyndontavares <integraldominio@gmail.com>
2022-07-31 20:58:06 -0300

    :construction:

--- src/test/java/com/mycompany/GameControllerTest.java
+++ src/test/java/com/mycompany/GameControllerTest.java
@@ -13,8 +13,14 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import com.mycompany.payload.request.LoginRequest;
+import com.mycompany.payload.request.SignupRequest;
 import com.mycompany.payload.response.RankingResponse;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -27,7 +33,7 @@ public class GameControllerTest {
 	int randomServerPort;
 
 	@Test
-	@Order(1) 
+	@Order(1)
 	void teste_acesso_nao_autorizado() throws URISyntaxException {
 
 		Exception exception = assertThrows(Exception.class, () -> {
@@ -43,6 +49,52 @@ public class GameControllerTest {
 		assertTrue(actualMessage.contains(expectedMessage));
 
 	}
- 
+
+	/**
+	 * IMPORTANTE: 
+	 * 
+	 * Por usar JWT em Cookie e BD em memória, faz-se necessário, para acessar rotas com segurança:
+	 * 
+	 * 1. Registrar novo jogador
+	 * 2. Fazer LOGIN
+	 * 3. Injetar COOKIE no REQUEST
+	 *   
+	 */
+	@Test
+	@Order(2)
+	void teste_iniciar_game() throws URISyntaxException {
+		
+		
+		//REGISTRO
+		URI uri = new URI("http://localhost:" + randomServerPort + "/api/auth/signup");
+		SignupRequest signupRequest = new SignupRequest();
+		signupRequest.setUsername("Jogador 2");
+		signupRequest.setPassword("123456");
+		testRestTemplate.postForEntity(uri, signupRequest, String.class);
+		
+		//LOGIN
+		uri = new URI("http://localhost:" + randomServerPort + "/api/auth/signin");
+		LoginRequest loginRequest = new LoginRequest();
+		loginRequest.setUsername("Jogador 2");
+		loginRequest.setPassword("123456");
+		testRestTemplate.postForEntity(uri, loginRequest, String.class);
+
+		//COOKIE 
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Cookie",
+				"mycompany=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJKb2dhZG9yIDIiLCJpYXQiOjE2NTkzMTA2NTIsImV4cCI6MTY1OTM5NzA1Mn0.yqrCajP8EwEHf-OQPQwcoJNCFw6zcCu8whGNHj1k-hviK5o4Lr_jDwnXXI2z-N-tjijtv_A8WaP8YxDlY6oLmQ; Path=/api; Max-Age=86400; Expires=Mon, 01 Aug 2022 23:39:22 GMT; HttpOnly");
+
+		uri = new URI("http://localhost:" + randomServerPort + "/api/game/start");
+
+		//REQUEST
+		ResponseEntity<String> response = testRestTemplate.exchange(uri, HttpMethod.POST,
+				new HttpEntity<String>(headers), String.class);
+
+		String expectedMessage = "Game Iniciado!";
+		String actualMessage = response.getBody();
+
+		assertTrue(actualMessage.contains(expectedMessage));
+
+	}
 
 }
